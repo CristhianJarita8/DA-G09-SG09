e4e40c859b7a983adb820c29775f55f6fe3d22b4
Cristhian Jarita <142757911+CristhianJarita8@users.noreply.github.com>
2024-09-24 22:31:42 -0500

    Add files via upload

---
+++ Semana 4/Codigo.js
@@ -0,0 +1,27 @@
+var num = 54;
+var nombre = "Julio";
+var valor = true;
+
+var arregloA = [22,33,44];
+var arregloB = ["abc", "efg"];
+
+if(num>30){
+    console.log("OK")
+}else{
+    console.log("ERROR")
+}
+
+for(i=1; i<5; i++){
+    console.log(1)
+}
+
+function sumar(num1,num2){
+    return num1+num2;
+}
+
+rpta = sumar(22,33)
+console.log(rpta)
+
+objeto1 = {nombre:"hector", edad=22, direccion:"Arequipa"}
+
+console.log()
