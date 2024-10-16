26520ca7bee0edfa0c57e377c876120083114003
Cristhian Jarita <142757911+CristhianJarita8@users.noreply.github.com>
2024-10-16 18:24:57 -0500

    Update index.js

--- Semana 6/sg9-react/src/index.js
+++ Semana 6/sg9-react/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Layout from "./Paginas/Layout";
+import Home from "./Paginas/Home";
+import Blogs from "./Paginas/Blogs";
+import Contact from "./Paginas/Contact";
+import NoPage from "./Paginas/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routers>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routers>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
