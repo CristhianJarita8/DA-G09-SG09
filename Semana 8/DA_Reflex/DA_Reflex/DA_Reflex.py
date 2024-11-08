e6f8eea579063304474bc5b108d3b1f4f65c95c5
Cristhian Jarita <cristhian.jarita@ucsm.edu.pe>
2024-11-07 20:48:38 -0500

    Nueva Actividad

--- Semana 8/DA_Reflex/DA_Reflex/DA_Reflex.py
+++ Semana 8/DA_Reflex/DA_Reflex/DA_Reflex.py
@@ -1,39 +1,64 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
 
-from rxconfig import config
+"""class State(rx.State):
+    count: int = 0
 
+    def incrementar(self):
+        self.count += 1
 
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+    def disminuir(self):
+        self.count -= 1
+        
+def index():
+    return rx.hstack(
+        rx.button(
+            "Disminuir",
+            color_scheme="ruby",
+            on_click=State.disminuir,
         ),
-        rx.logo(),
+        rx.heading(State.count, font_size="2em"),
+        rx.button(
+            "Incrementar",
+            color_scheme="grass",
+            on_click=State.incrementar,
+        ),
+        spacing="4",
+    )
+app = rx.App()
+app.add_page(index)"""
+
+class EstadoTareas(rx.State):
+    tareas: list[str] = []
+    
+    def agregar_tarea(self, tarea:str):
+        if tarea:
+            self.tareas.append(tarea)
+    
+def agregar_tarea():
+    nueva_tarea = rx.input(placeholder="Agregar tarea...", id="input_tarea")
+
+    return rx.hstack(
+        nueva_tarea,
+        rx.button(
+            "Agregar",
+            on_click=lambda: EstadoTareas.agregar_tarea(nueva_tarea.value),    
+        ),
+        spacing="2",
     )
 
+def lista_tareas():
+    return rx.vstack(
+        *[rx.text(tarea, font_size="1.2em") for tarea in EstadoTareas.tareas],
+        spacing="2",
+    )
+
+def index():
+    return rx.vstack(
+        rx.heading("Lista de Tareas", font_size="2em"),
+        lista_tareas(),
+        agregar_tarea(),
+        spacing="4",
+    )
 
 app = rx.App()
 app.add_page(index)
